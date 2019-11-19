a3f47da165efea562de528bbdf506ab682e22d19
tom5167 <tom.jose.25@gmail.com>
2019-11-19 15:34:33 -0500

    POJO added

---
+++ src/patientCarePOJO/Bill.java
@@ -0,0 +1,16 @@
+package patientCarePOJO;
+
+import patientCareConstants.CommonConstants;
+
+public class Bill {
+	private int eventId = CommonConstants.ZERO;
+
+	public int getEventId() {
+		return eventId;
+	}
+
+	public void setEventId(int eventId) {
+		this.eventId = eventId;
+	}
+	
+}
---
+++ src/patientCarePOJO/Staff.java
@@ -0,0 +1,16 @@
+package patientCarePOJO;
+
+import patientCareConstants.CommonConstants;
+
+public class Staff {
+	private int eventId = CommonConstants.ZERO;
+
+	public int getEventId() {
+		return eventId;
+	}
+
+	public void setEventId(int eventId) {
+		this.eventId = eventId;
+	}
+	
+}
